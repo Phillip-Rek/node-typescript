eda4145a5b94b820f290a6d9c01e3abc4d8aa3a7
Phillip-Rek <philliprekhotho@gmail.com>
2024-06-20 15:23:17 +0200

    createTable method for creating users table

--- src/models/users.ts
+++ src/models/users.ts
@@ -6,7 +6,8 @@ export class Users {
     private static tableName: string;
 
     private constructor() { }
-    query = DatabaseConnection.query;
+
+    static query = DatabaseConnection.query;
 
     static getInstance(tableName: string) {
         Users.tableName = tableName;
@@ -23,4 +24,23 @@ export class Users {
             return this.instance
         }
     }
+
+    static createTable = () => {
+        Users.query(`SELECT * FROM ${Users.tableName}`)
+            .then(res => {
+                console.log(`TABLE, ${Users.tableName} EXIST`);
+            })
+            .catch(err => {
+                console.log(`CREATED TABLE, ${Users.tableName}`);
+                Users.query(`CREATE TABLE \`${Users.tableName}\`(
+                \`id\` int NOT NULL AUTO_INCREMENT,
+                \`name\` varchar(150) NOT NULL,
+                \`password\` varchar() NOT NULL,
+                \`email\` varchar(150) NOT NULL,
+                \`date\` Date DEFAULT current_timestamp(),
+                PRIMARY KEY (\`id\`)
+                ) ENGINE=InnoDB COLLATE=utf8_general_ci;`);
+            })
+    }
+
 }
