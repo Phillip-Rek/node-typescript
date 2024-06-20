f40c7382612b595e9a1ee60c2bc19fafa2eb194c
Phillip-Rek <philliprekhotho@gmail.com>
2024-06-20 15:20:36 +0200

    getInstance method
    this method returns the instance of the class, it is to ensure that there is only one instance of the class

--- src/models/users.ts
+++ src/models/users.ts
@@ -8,5 +8,19 @@ export class Users {
     private constructor() { }
     query = DatabaseConnection.query;
 
+    static getInstance(tableName: string) {
+        Users.tableName = tableName;
+        if (this.instance) {
+            return this.instance
+        }
+        else {
+            DatabaseConnection.getConnection([Users.createTable]);
+            Users.createTable();
 
+            console.log("Created Users Instance");
+
+            this.instance = new Users();
+            return this.instance
+        }
+    }
 }
