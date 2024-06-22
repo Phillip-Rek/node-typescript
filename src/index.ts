f6f5068106a373374fe8b455e1f37e06d026c831
Phillip-Rek <philliprekhotho@gmail.com>
2024-06-22 19:29:54 +0200

    register quotesRouter and indexRouter

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 import express from "express";
-
+import { indexRouter } from "./routes/index";
+import { quotesRoutes } from "./routes/quotes";
+import { engine } from "perthite-2";
 
 const app = express();
+
+
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/quotes", quotesRoutes);
+
+app.use("/", indexRouter);
+
+app.all("*", (req, res) => {
+    res.status(404).send("Page Not Found")
+})
 
 app.listen(3000);
