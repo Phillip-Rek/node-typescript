6fa68db5ba8d936594d19163c0a126c4302da970
Phillip-Rek <philliprekhotho@gmail.com>
2024-06-23 18:16:33 +0200

    make methods async in abstract Controller

--- src/controllers/posts.controller.ts
+++ src/controllers/posts.controller.ts
@@ -2,15 +2,26 @@ import { Request, Response } from "express";
 import { postsServices } from "../services/posts.services";
 
 export abstract class Controller {
-    index(req: Request, res: Response) { throw new Error("Method not implemented"); };
-    createOne(req: Request, res: Response) { throw new Error("Method not implemented"); };
-    updateOne(req: Request, res: Response) { throw new Error("Method not implemented"); };
-    deleteOne(req: Request, res: Response) { throw new Error("Method not implemented"); };
+    async index(req: Request, res: Response): Promise<unknown> { throw new Error("Method not implemented"); };
+    async createOne(req: Request, res: Response): Promise<unknown> { throw new Error("Method not implemented"); };
+    async updateOne(req: Request, res: Response): Promise<unknown> { throw new Error("Method not implemented"); };
+    async deleteOne(req: Request, res: Response): Promise<unknown> { throw new Error("Method not implemented"); };
 }
 
 export class PostsController implements Controller {
-    updateOne(req: Request, res: Response): void {
-        throw new Error("Method not implemented.");
+    updateOne(req: Request, res: Response) {
+        if (!req.body.title || !req.body.description || !req.body.body) {
+            return res.status(400).send("Bad Request");
+        }
+
+        const post = {
+            title: req.body.title,
+            description: req.body.description,
+            body: req.body.body,
+            date: new Date()
+        }
+
+        return res.send(await postsServices.createOne(post));
     }
     deleteOne(req: Request, res: Response): void {
         throw new Error("Method not implemented.");
@@ -21,7 +32,6 @@ export class PostsController implements Controller {
     }
 
     async createOne(req: Request, res: Response) {
-        // throw new Error("Method not implemented.");
         if (!req.body.title || !req.body.description || !req.body.body) {
             return res.status(400).send("Bad Request");
         }
