eb1399e60549a62ab8d00623fe278d93d569443b
Phillip-Rek <philliprekhotho@gmail.com>
2024-06-23 15:27:57 +0200

    move methodOverride callback to a separate file

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ const methodOverride = require('method-override');
 import { indexRouter } from "./routes/index";
 import { quotesRoutes } from "./routes/quotes";
 import { engine } from "perthite-2";
+import { overridePostMethod } from "./middlewares/method-override";
 
 const app = express();
 
@@ -13,14 +14,7 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride((req: { body: { _method: any; }; }, res: any) => {
-    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-        // look in urlencoded POST bodies and delete it
-        var method = req.body._method
-        delete req.body._method
-        return method
-    }
-}))
+app.use(methodOverride(overridePostMethod))
 
 app.use("/quotes", quotesRoutes);
 
