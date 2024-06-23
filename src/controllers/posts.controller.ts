448e8efbfc52ab5775ff2d07ed64022e18428f87
Phillip-Rek <philliprekhotho@gmail.com>
2024-06-23 17:35:56 +0200

    index method to render all posts

--- src/controllers/posts.controller.ts
+++ src/controllers/posts.controller.ts
@@ -3,4 +3,7 @@ import { postsServices } from "../services/posts.services";
 
 export class PostsController {
 
+    async index(req: Request, res: Response) {
+        return res.render("posts", { quotes: await postsServices.getAll() });
+    }
 }
