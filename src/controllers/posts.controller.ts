3b66572587e29f823bd19d3007afdb8c5ff1beb0
Phillip-Rek <philliprekhotho@gmail.com>
2024-06-23 17:55:33 +0200

    update and delete methods in posts controller

--- src/controllers/posts.controller.ts
+++ src/controllers/posts.controller.ts
@@ -1,9 +1,33 @@
 import { Request, Response } from "express";
 import { postsServices } from "../services/posts.services";
 
+
+
 export class PostsController {
+    updateOne(req: Request, res: Response): void {
+        throw new Error("Method not implemented.");
+    }
+    deleteOne(req: Request, res: Response): void {
+        throw new Error("Method not implemented.");
+    }
 
     async index(req: Request, res: Response) {
         return res.render("posts", { quotes: await postsServices.getAll() });
+    }
+
+    async createOne(req: Request, res: Response) {
+        throw new Error("Method not implemented.");
+        // if (!(req.body.name && req.body.message)) {
+        //     return res.status(400).send("Bad Request");
+        // }
+
+        // const post = {
+        //     name: req.body.name,
+        //     message: req.body.message,
+        //     date: new Date()
+        // }
+
+        // return res.send(await postsServices.createOne(post));
+
     }
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,5 +10,4 @@ export {
 }
 
 
-// app.get("/", (req, { send }) => { send("hello world") })
 
