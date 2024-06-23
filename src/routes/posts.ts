4b3431faa9eae477f3d725b5a47d783b6adf0540
Phillip-Rek <philliprekhotho@gmail.com>
2024-06-23 22:21:59 +0200

    populate posts router

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
+import { PostsController } from "../controllers/posts.controller";
 
 
 const PostsRoute = Router();
 
+const { updateOne, deleteOne, index, createOne } = new PostsController();
 
+PostsRoute.get("/", index);
+
+PostsRoute.post("/", createOne);
+
+PostsRoute.put("/:id", updateOne);
+
+PostsRoute.delete("/:id", deleteOne);
 
 export {
     PostsRoute
