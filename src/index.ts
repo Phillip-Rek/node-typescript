bcc276aaf0c2c9052614ef2916bb40a6c92e80e9
Phillip-Rek <philliprekhotho@gmail.com>
2024-06-23 13:46:25 +0200

    import and use methodOverride

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+const methodOverride = require('method-override');
 import { indexRouter } from "./routes/index";
 import { quotesRoutes } from "./routes/quotes";
 import { engine } from "perthite-2";
@@ -12,6 +13,14 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride((req: { body: { _method: any; }; }, res: any) => {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        var method = req.body._method
+        delete req.body._method
+        return method
+    }
+}))
 
 app.use("/quotes", quotesRoutes);
 
@@ -21,4 +30,5 @@ app.all("*", (req, res) => {
     res.status(404).send("Page Not Found")
 })
 
+process.env.NODE_ENV = "development";
 app.listen(3000);
