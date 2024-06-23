ca1116a1f69752d402379bf58f4b621bc9a17793
Phillip-Rek <philliprekhotho@gmail.com>
2024-06-23 13:41:28 +0200

    update method from Quotes controller

--- src/models/quotes.ts
+++ src/models/quotes.ts
@@ -43,6 +43,14 @@ class Quotes extends Table {
         })
     }
 
+    async update(quote: Omit<Quote, "date">) {
+        return new Promise((resolve, reject) => {
+            this.query(`UPDATE ${this.tableName} SET \`name\`=?, \`message\`=? WHERE \`id\`=?`, [quote.name, quote.message, quote.id])
+                .then(res => { resolve(res) })
+                .catch(err => { reject(err) })
+        })
+    }
+
 }
 
 export const quotes = new Quotes();
