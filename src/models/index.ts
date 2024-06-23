64b5ebfdb96b12461811c94ab64be946f4c76184
Phillip-Rek <philliprekhotho@gmail.com>
2024-06-23 15:36:15 +0200

    move Table class to models/index.ts

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,5 @@
 import * as mysql from "mysql";
 import { brand } from "../brand";
-import { Table } from "./table";
-
 export class DatabaseConnection {
 
     // static ready = false;
@@ -106,4 +104,34 @@ export class DatabaseConnection {
             reject(err)
         }
     })
+}
+
+
+export class Table {
+    tableName: string = "";
+    structure: string[] = [];
+    constructor() {
+        DatabaseConnection.getConnection([this]);
+    }
+
+    async createTable(tableStructure: string): Promise<string> {
+        return new Promise((resolve, reject) => {
+            this.query(`SELECT * FROM ${this.tableName}`)
+                .then(res => {
+                    console.log(`TABLE, ${this.tableName} EXIST`);
+
+                    resolve(`TABLE, ${this.tableName} EXIST`);
+                })
+                .catch(err => {
+                    console.log(`CREATED TABLE, ${this.tableName}`);
+                    this.query(`CREATE TABLE \`${this.tableName}\`(${tableStructure.replace(/[ \s\n]+/g, " ")}
+                    ) ENGINE=InnoDB COLLATE=utf8_general_ci;`);
+
+                    resolve(`CREATED TABLE, ${this.tableName}`);
+                })
+        })
+    }
+
+    query = DatabaseConnection.query;
+
 }
