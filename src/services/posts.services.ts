deb5a773608bdeae608933a5f81575083f60e0f8
Phillip-Rek <philliprekhotho@gmail.com>
2024-06-23 17:19:09 +0200

    method to get all posts from posts model

--- src/services/posts.services.ts
+++ src/services/posts.services.ts
@@ -1,2 +1,10 @@
 import { posts, Post } from "../models/posts";
 
+class PostsServices {
+
+    async getAll() {
+        return posts.getAll();
+    }
+}
+
+const postsServices = new PostsServices();
