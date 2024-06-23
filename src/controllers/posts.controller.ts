991436b7a0214bc7e44b84e586226076e7ce1d9d
Phillip-Rek <philliprekhotho@gmail.com>
2024-06-23 22:17:07 +0200

    fix: add id to post object, and validate it

--- src/controllers/posts.controller.ts
+++ src/controllers/posts.controller.ts
@@ -1,32 +1,23 @@
 import { Request, Response } from "express";
 import { postsServices } from "../services/posts.services";
+import { posts } from "../models/posts";
 
 export abstract class Controller {
-    async index(req: Request, res: Response): Promise<unknown> { throw new Error("Method not implemented"); };
-    async createOne(req: Request, res: Response): Promise<unknown> { throw new Error("Method not implemented"); };
-    async updateOne(req: Request, res: Response): Promise<unknown> { throw new Error("Method not implemented"); };
-    async deleteOne(req: Request, res: Response): Promise<unknown> { throw new Error("Method not implemented"); };
+    async index(req: Request, res: Response): Promise<unknown> {
+        throw new Error("Method not implemented");
+    };
+    async createOne(req: Request, res: Response): Promise<unknown> {
+        throw new Error("Method not implemented");
+    };
+    async updateOne(req: Request, res: Response): Promise<unknown> {
+        throw new Error("Method not implemented");
+    };
+    async deleteOne(req: Request, res: Response): Promise<unknown> {
+        throw new Error("Method not implemented");
+    };
 }
 
 export class PostsController implements Controller {
-    async updateOne(req: Request, res: Response) {
-        if (!req.body.title || !req.body.description || !req.body.body) {
-            return res.status(400).send("Bad Request");
-        }
-
-        const post = {
-            title: req.body.title,
-            description: req.body.description,
-            body: req.body.body,
-            date: new Date()
-        }
-
-        return res.send(await postsServices.createOne(post));
-    }
-
-    async deleteOne(req: Request, res: Response) {
-        throw new Error("Method not implemented.");
-    }
 
     async index(req: Request, res: Response) {
         return res.render("posts", { quotes: await postsServices.getAll() });
@@ -45,6 +36,32 @@ export class PostsController implements Controller {
         }
 
         return res.send(await postsServices.createOne(post));
+    }
 
+    async updateOne(req: Request, res: Response) {
+        const id = parseInt(req.params.id);
+
+        if (!req.body.title || !req.body.description || !req.body.body || Number.isNaN(id)) {
+            return res.status(400).send("Bad Request");
+        }
+
+
+        const post = {
+            title: req.body.title,
+            description: req.body.description,
+            body: req.body.body,
+            date: new Date(),
+            id
+        }
+
+        return res.send(await postsServices.updateOne(post));
+    }
+
+    async deleteOne(req: Request, res: Response) {
+        const id = parseInt(req.params.id);
+
+        if (Number.isNaN(id)) { return res.status(400).send("Invalid id") }
+
+        return res.send(await postsServices.deleteOne(id));
     }
 }
