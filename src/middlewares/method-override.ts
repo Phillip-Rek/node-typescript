19ff19f58e2924bd467da80954c0500fc0c80e83
Phillip-Rek <philliprekhotho@gmail.com>
2024-06-23 15:26:54 +0200

    overrideMethod middleware

---
+++ src/middlewares/method-override.ts
@@ -0,0 +1,8 @@
+export function overridePostMethod(req: { body: { _method: any; }; }, res: any) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        var method = req.body._method
+        delete req.body._method
+        return method
+    }
+}
