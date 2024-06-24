8b0b783113d3b8d8d9a57acd3b0b4ef4780488e1
Phillip-Rek <philliprekhotho@gmail.com>
2024-06-24 21:37:25 +0200

    utilize singleton controller and service class

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { PostsController } from "../controllers/posts.controller";
-import { postsServices } from "../services/posts.services";
+import { PostsServices } from "../services/posts.services";
+import { posts } from "../models/posts";
 
 const PostsRouter = Router();
 
-const postsController = PostsController.getInstance(postsServices);
+const postsController = PostsController.getInstance(PostsServices.getInstance(posts));
 
 PostsRouter.get("/", postsController.index);
 
