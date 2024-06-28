b4b45cf1fc3d980f39014036c5c369cf93a12911
Phillip-Rek <philliprekhotho@gmail.com>
2024-06-28 15:46:34 +0200

    add update method to test update in Posts service

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -5,7 +5,21 @@ import { posts } from "../models/posts";
 
 const PostsRouter = Router();
 
-const postsController = PostsController.getInstance(PostsServices.getInstance(posts));
+const postsServices = new PostsServices(posts)
+
+const postsController = PostsController.getInstance(postsServices);
+
+const update = async () => {
+    console.log(
+        await postsServices.updateOne(1,
+            {
+                title: "the time is 15:31",
+                date: new Date().toISOString().slice(0, 19).replace('T', ' ')
+            })
+    )
+}
+
+setTimeout(update, 3000);
 
 PostsRouter.get("/", postsController.index);
 
