0138a97deda32c3fb9c150120abb99f194e82eef
Phillip-Rek <philliprekhotho@gmail.com>
2024-06-28 15:41:20 +0200

    fix: make readOne easy to use with any property, like id

--- src/controllers/posts.controller.ts
+++ src/controllers/posts.controller.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from "express";
 import { Controller } from "./controller";
-import { postsServices, PostsServices } from "../services/posts.services";
+import { PostsServices } from "../services/posts.services";
 
 export class PostsController implements Controller {
-
-    static instance?: PostsController;
+    private static instance?: PostsController;
     private static postsServices: PostsServices;
 
     private constructor(postsServices: PostsServices) {
         PostsController.postsServices = postsServices;
+
+
     }
 
     static getInstance(service: PostsServices) {
@@ -31,7 +32,7 @@ export class PostsController implements Controller {
 
         if (Number.isNaN(id)) { return res.status(400).send("Bad Request"); }
 
-        return res.send(await PostsController.postsServices.getOne(id));
+        return res.send(await PostsController.postsServices.getOne({ id }));
     }
 
     async createOne(req: Request, res: Response) {
@@ -43,7 +44,7 @@ export class PostsController implements Controller {
             title: req.body.title,
             description: req.body.description,
             body: req.body.body,
-            date: new Date()
+            date: new Date().toISOString()
         }
 
         return res.send(await PostsController.postsServices.createOne(post));
@@ -52,16 +53,16 @@ export class PostsController implements Controller {
     async updateOne(req: Request, res: Response) {
         const id = parseInt(req.params.id);
 
-        if (!req.body.title || !req.body.description || !req.body.body || Number.isNaN(id)) {
+        if (!req.body.date || !req.body.title || !req.body.description || !req.body.body || Number.isNaN(id)) {
             return res.status(400).send("Bad Request");
         }
 
         return res.send(
-            await PostsController.postsServices.updateOne({
+            await PostsController.postsServices.updateOne(id, {
                 title: req.body.title,
                 description: req.body.description,
                 body: req.body.body,
-                id
+                date: req.body.date
             })
         );
     }
